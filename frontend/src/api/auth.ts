9b9b288aa5ade64fc693f8ab5656f3fc0dc070ab
Priyank Sharma <priyank22022006@gmail.com>
2026-04-03 15:29:24 +0530

    update api url to render

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const BASE_URL = 'https://vi-notes-backend-qloi.onrender.com/';
 
 interface AuthResponse {
   message: string;
